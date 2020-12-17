6a1d5d14989a71addf2036e69a32818604613501
Phil MacEachron <phil@mbp.fios-router.home>
2020-12-17 10:39:37 -0500

    initial commit

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Phil MacEachron',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
